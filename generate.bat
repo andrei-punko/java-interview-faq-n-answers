8b9bccd4015e5c78c4b028606a726a2e111736f5
Andrei Punko <andd3dfx@gmail.com>
2022-05-24 16:50:07 +0300

    Add content

--- generate.bat
+++ generate.bat
@@ -2,3 +2,7 @@
 del *.html
 
 java -jar bin/github-pages-content-generator.jar src/content.txt bin/template.html index.html
+java -jar bin/github-pages-content-generator.jar "src/answers-1.txt" bin/template.html answers-1.html
+java -jar bin/github-pages-content-generator.jar "src/answers-2.txt" bin/template.html answers-2.html
+java -jar bin/github-pages-content-generator.jar "src/answers-3.txt" bin/template.html answers-3.html
+java -jar bin/github-pages-content-generator.jar "src/answers-4.txt" bin/template.html answers-4.html
