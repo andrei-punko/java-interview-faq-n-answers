d1398f8bd0b7837d3ce67606f6216c874ac684b3
Andrei Punko <andd3dfx@gmail.com>
2022-06-06 11:14:07 +0300

    Merge 'Answers' page into 'Main' page

--- generate.bat
+++ generate.bat
@@ -2,4 +2,3 @@
 del *.html
 
 java -jar bin/github-pages-content-generator.jar src/content.txt bin/template.html index.html
-java -jar bin/github-pages-content-generator.jar "src/answers-1.txt" bin/template.html answers-1.html
