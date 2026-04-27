55ba78fff7e164bf5d6146b59c789cb4df0c4f41
Andrei Punko <andd3dfx@gmail.com>
2026-04-27 22:54:47 +0300

    Update generate-n-commit-n-push.bat

--- generate-n-commit-n-push.bat
+++ generate-n-commit-n-push.bat
@@ -1,3 +1,9 @@
+@echo off
+if not exist "github-pages-content-generator\target\github-pages-content-generator.jar" (
+  echo ERROR: JAR not found: github-pages-content-generator\target\github-pages-content-generator.jar
+  echo Build it first, e.g. cd github-pages-content-generator ^&^& mvn package
+  exit /b 1
+)
 
 call generate.bat
 
